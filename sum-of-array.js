cfe640984010a439b23c167d32decc9d225fefe4
hammadsadi <hammad.sadi@yahoo.com>
2024-01-30 16:04:34 +0600

    Sum Of Array

---
+++ sum-of-array.js
@@ -0,0 +1,7 @@
+// Sum Of Array
+let numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+let sumOfArray = 0;
+for (let i = 0; i < numbers.length; i++) {
+  sumOfArray += numbers[i];
+  console.log(sumOfArray);
+}
