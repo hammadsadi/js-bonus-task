c154d0d029b371746ec3d46bc7dc497dd28945bf
hammadsadi <hammad.sadi@yahoo.com>
2024-01-30 12:09:27 +0600

    access-array-element-using-loop

--- access-array-element-using-loop.js
+++ access-array-element-using-loop.js
@@ -1,1 +1,6 @@
-// access-array-element-using-loop.js
+// access-array-element-using-loop
+
+let userList = ["Hammad Sadi", "Minhaj", "Moaimin", "Zakiya", "Ashraf"];
+for (let i = 0; i < userList.length; i++) {
+  console.log(userList[i]);
+}
